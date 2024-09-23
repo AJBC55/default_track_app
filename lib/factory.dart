a4e7763de7bf9a96dd6e35be34a10547f9006f9b
Andrew Chapman <andrew.j.b.chapman@gmail.com>
2024-09-22 17:50:00 -0700

    setup

---
+++ lib/factory.dart
@@ -0,0 +1,3 @@
+// this File should contain all of the track speficic information there should be no track spesific informaton in any other file other than here or a media file
+// !!!All app speficic data should be passed to each part of the app Ex events timing through dependancy injection!!!
+
---
+++ lib/readme.txt
@@ -0,0 +1,3 @@
+This File should contain all of the track speficic information there should be no track spesific informaton in any other file other than here or a media file
+!!!All app speficic data should be passed to each part of the app through dependancy injection!!!
+
